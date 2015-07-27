3d8a660711b86617c0c6e6151734a5f7b0509d2e
aaronbutler <aaron@thoroughcio.com>
2015-07-26 20:45:44 -0400

    add toasty buttons and color

---
+++ MyApplication/app/src/androidTest/java/com/example/udacity/myappportfolio/MainActivityTest.java
@@ -0,0 +1,13 @@
+package com.example.udacity.myappportfolio;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by ben on 7/26/2015.
+ */
+public class MainActivityTest extends TestCase {
+
+    public void testToastMe() throws Exception {
+
+    }
+}
